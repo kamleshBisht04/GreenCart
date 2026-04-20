eca37f58858c0f28cd78c728c2015e884bffa215
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-04-20 17:34:22 +0530

    add product.jsx complete

--- client/src/pages/seller/AddProduct.jsx
+++ client/src/pages/seller/AddProduct.jsx
@@ -1,7 +1,177 @@
-import React from "react";
+import { useState } from "react";
+import { assets, categories } from "../../assets/assets";
 
 const AddProduct = () => {
-  return <div>AddProduct</div>;
+  const [product, setproduct] = useState({
+    name: "",
+    description: "",
+    category: "",
+    price: "",
+    offerPrice: "",
+    weight: "",
+    images: [],
+  });
+
+  const handleImageChange = (e, index) => {
+    const file = e.target.files[0];
+    setproduct((prev) => {
+      const newImages = [...prev.images];
+      newImages[index] = file;
+      return { ...prev, images: newImages };
+    });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setproduct((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="no-scrollbar flex h-[95vh] flex-1 flex-col justify-between overflow-y-scroll">
+      <form
+        onSubmit={onSubmitHandler}
+        className="max-w-lg space-y-5 p-4 md:p-10"
+      >
+        <div>
+          <p className="text-base font-medium">Product Image</p>
+          <div className="mt-2 flex flex-wrap items-center gap-3">
+            {Array(4)
+              .fill("")
+              .map((_, index) => (
+                <label key={index} htmlFor={`image${index}`}>
+                  <input
+                    onChange={(e) => handleImageChange(e, index)}
+                    accept="image/*"
+                    type="file"
+                    id={`image${index}`}
+                    hidden
+                  />
+                  <img
+                    className="max-w-24 cursor-pointer"
+                    src={
+                      product.images[index]
+                        ? URL.createObjectURL(product.images[index])
+                        : assets.upload_area
+                    }
+                    alt="uploadArea"
+                    width={100}
+                    height={100}
+                  />
+                </label>
+              ))}
+          </div>
+        </div>
+        <div className="flex max-w-md flex-col gap-1">
+          <label className="text-base font-medium" htmlFor="product-name">
+            Product Name
+          </label>
+          <input
+            name="name"
+            onChange={handleChange}
+            value={product.name}
+            id="product-name"
+            type="text"
+            placeholder="Type here"
+            className="rounded border border-gray-500/40 px-3 py-2 outline-none md:py-2.5"
+            required
+          />
+        </div>
+        <div className="flex max-w-md flex-col gap-1">
+          <label
+            className="text-base font-medium"
+            htmlFor="product-description"
+          >
+            Product Description
+          </label>
+          <textarea
+            name="description"
+            onChange={handleChange}
+            value={product.description}
+            id="product-description"
+            rows={4}
+            className="resize-none rounded border border-gray-500/40 px-3 py-2 outline-none md:py-2.5"
+            placeholder="Type here"
+          ></textarea>
+        </div>
+        <div className="flex w-full flex-col gap-1">
+          <label className="text-base font-medium" htmlFor="category">
+            Category
+          </label>
+          <select
+            name="category"
+            onChange={handleChange}
+            value={product.category}
+            id="category"
+            className="rounded border border-gray-500/40 px-3 py-2 outline-none md:py-2.5"
+          >
+            <option value="">Select Category</option>
+            {categories.map((item, index) => (
+              <option key={index} value={item.path}>
+                {item.path}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex flex-wrap items-center gap-5">
+          <div className="flex w-32 flex-1 flex-col gap-1">
+            <label className="text-base font-medium" htmlFor="product-price">
+              Product Price
+            </label>
+            <input
+              name="price"
+              onChange={handleChange}
+              value={product.price}
+              id="product-price"
+              type="number"
+              placeholder="0"
+              className="rounded border border-gray-500/40 px-3 py-2 outline-none md:py-2.5"
+              required
+            />
+          </div>
+          <div className="flex w-32 flex-1 flex-col gap-1">
+            <label className="text-base font-medium" htmlFor="offer-price">
+              Offer Price
+            </label>
+            <input
+              name="offerPrice"
+              onChange={handleChange}
+              value={product.offerPrice}
+              id="offer-price"
+              type="number"
+              placeholder="0"
+              className="rounded border border-gray-500/40 px-3 py-2 outline-none md:py-2.5"
+              required
+            />
+          </div>
+          <div className="flex w-32 flex-1 flex-col gap-1">
+            <label className="text-base font-medium" htmlFor="weight">
+              Weight
+            </label>
+            <input
+              name="weight"
+              onChange={handleChange}
+              value={product.weight}
+              id="weight"
+              type="number"
+              placeholder="0"
+              className="rounded border border-gray-500/40 px-3 py-2 outline-none md:py-2.5"
+              required
+            />
+          </div>
+        </div>
+        <button className="bg-primary rounded px-8 py-2.5 font-medium text-white">
+          ADD
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AddProduct;
