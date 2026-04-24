87e540699ce09f800a995172047d77043d605df8
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-04-24 22:42:27 +0530

    join express to mongoose

---
+++ server/configs/db.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.MONGODB_URI.replace(
+  '<PASSWORD>',
+  process.env.DB_PASSWORD,
+);
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on('connected', () => {
+      console.log('Database  connected !');
+    });
+    await mongoose.connect(DB);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import connectDB from './configs/db.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
+await connectDB();
 
 // Allow multiple orign
 const allowedOrigins = ['https://localhost:5173'];
@@ -13,10 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: allowedOrigins, Credential: true }));
 
-app.get('/', (req, res) => res.send('API is Working!!'));
-
-
-cd
+app.get('/', (req, res) => res.send('API is Working!!!'));
 
 app.listen(port, () => {
   console.log(`Server is runing on http://localhost:${port}`);
