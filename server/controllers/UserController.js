b956c829bf4f9d13ec00bf4fd6ca1d535e714ff0
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-04-25 19:48:06 +0530

    creating the userController.js and writing the controller to register the user '

---
+++ server/controllers/UserController.js
@@ -0,0 +1,69 @@
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+//Generate token
+const generateToken = (id) => {
+  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: '7d',
+  });
+};
+
+// Register User
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    // validation
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: 'All fields are required',
+      });
+    }
+    // already exist
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({
+        success: false,
+        message: 'User already exists',
+      });
+    }
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    // Generate token
+    const token = generateToken(user._id);
+
+    // send cookie
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    // Response
+    res.status(201).json({
+      success: true,
+      message: 'User registered successfully',
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    });
+  } catch (error) {
+    console.error('Register Error:', error.message);
+
+    res.status(500).json({
+      success: false,
+      message: 'Server error',
+    });
+  }
+};
