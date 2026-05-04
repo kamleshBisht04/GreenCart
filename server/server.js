89554584e27c1af477f0874aa6e5f7d5956de455
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-05-04 16:36:46 +0530

    refector the server and start server async function

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+
+// DB & Configs
 import connectDB from './configs/db.js';
+import connectCloudinary from './configs/cloudinary.js';
+
+// Routes
 import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
-import { connect } from 'mongoose';
-import connectCloudinary from './configs/cloudinary.js';
-import productRoucter from './routes/productRoute.js';
+import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import addressRouter from './routes/addressRoute.js';
 import orderRouter from './routes/orderRoute.js';
@@ -15,33 +18,50 @@ import contactRouter from './routes/contactRoutes.js';
 import newsletterRouter from './routes/newsletterRoutes.js';
 
 const app = express();
-const port = process.env.PORT || 4000;
-await connectDB();
-await connectCloudinary();
+const PORT = process.env.PORT || 4000;
 
-// Allow multiple orign
-const allowedOrigins = ['http://localhost:5173'];
+// Connect DB & Cloudinary
+const startServer = async () => {
+  try {
+    await connectDB();
+    await connectCloudinary();
+    console.log('✅ DB & Cloudinary Connected');
 
-// Middelewere configuration
+    app.listen(PORT, () => {
+      console.log(` Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(' Server Start Failed:', error.message);
+    process.exit(1);
+  }
+};
+
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  }),
+);
 
-app.get('/', (req, res) => res.send('API is Working!!!'));
+// Health Check Route
+app.get('/', (req, res) => {
+  res.send('API is Working ');
+});
 
+//  API Routes
 app.use('/api/user', userRouter);
 app.use('/api/seller', sellerRouter);
-app.use('/api/product', productRoucter);
+app.use('/api/product', productRouter);
 app.use('/api/cart', cartRouter);
-app.use('/api/contact', contactRouter);
-app.use('/api/newsletter', newsletterRouter);
-
 app.use('/api/address', addressRouter);
 app.use('/api/order', orderRouter);
 app.use('/api/payment', paymentRouter);
+app.use('/api/contact', contactRouter);
+app.use('/api/newsletter', newsletterRouter);
 
-
-app.listen(port, () => {
-  console.log(`Server is runing on http://localhost:${port}`);
-});
+// Start Server
+startServer();
