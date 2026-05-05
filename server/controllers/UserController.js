c6c207e6e08cac04ae0a47c37b7c02680d08d88a
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-05-05 17:15:02 +0530

    fix the cookes path in userController

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -38,6 +38,7 @@ export const registerUser = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      path: '/',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     // Response
@@ -96,6 +97,7 @@ export const login = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      path: '/', 
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -142,6 +144,7 @@ export const logout = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      path: '/', 
     });
 
     return res.status(200).json({
