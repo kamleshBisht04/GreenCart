f83d5b5d84eddc745a46f1d7d2e86a3bca5560ae
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-05-06 14:11:59 +0530

    project complete

--- server/configs/db.js
+++ server/configs/db.js
@@ -6,11 +6,11 @@ const connectDB = async () => {
   try {
     const uri = process.env.MONGODB_URI.replace('<PASSWORD>', process.env.DB_PASSWORD);
 
-    await mongoose.connect(uri); // पुराने ऑप्शन्स हटा दिए
+    await mongoose.connect(uri); 
     console.log('✅ MongoDB Connected');
   } catch (error) {
     console.error('❌ MongoDB Connection Error:', error.message);
-    process.exit(1); // अगर कनेक्शन फेल हो जाए तो सर्वर बंद कर दो
+    process.exit(1); 
   }
 };
 
