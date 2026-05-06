1ba09d470e81646180c5ea11106353c22b7465ef
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-05-06 22:20:51 +0530

    adding the loader in home screen

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/60 backdrop-blur-sm">
-      <div className="lds-roller">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className="relative min-h-[400px]">
+      <div className="flex min-h-[300px] items-center justify-center py-10">
+        <div className="lds-roller">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
   );
